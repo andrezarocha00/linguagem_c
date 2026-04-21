89ecd18a956ef1169c010a727d255c9ba5c1191e
A N D R E Z A    R O C H A <79232551+andrezarocha00@users.noreply.github.com>
2026-04-21 19:38:59 -0300

    Add files via upload
    Aula 1

---
+++ exer1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int ano = 1996;
+	printf("Nome: Andreza Rocha\nNascimento: %d", ano);
+	return 0;
+}
---
+++ exerc5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	char letra = 'A';
+	for(char letra = 'a'; letra <= 'z'; letra++){
+		printf(" %c", letra);
+}
+	return 0;
+}
